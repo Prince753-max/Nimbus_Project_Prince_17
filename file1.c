3667316fc0c7429fec63cb4066e0822f3eb0495c
umairrahman737-IAF <umairrahman737@gmail.com>
2025-11-26 14:21:36 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -28,5 +28,5 @@ typedef struct{
 Transaction*addTransaction(Transaction*transaction,int*count, Transaction newTrans);
 void monthlySummary(Transaction *transactions, int count, int month, int year, double *incomeTotal,
 double *expenseTotal);
-void categorySpending();
-ho0wemembwjhdbdjvjdjcvbiudjfdwf 
+void categorySpending
+
